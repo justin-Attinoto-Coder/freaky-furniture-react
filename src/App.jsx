6f58020fe0cfdad5e0831fd2991ac425261a30c3
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-02-25 18:07:04 +0100

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white p-4 mx-auto">
+        <h1 className="text-8xl text-sky-500">Hello, world!</h1>
+      </div>
+      <div>
+        <h2 className="text-3xl">Welcome to your Vite + React app</h2>
+      </div>
+      <div>
+        <button className="bg-amber-500 px-5 py-2 rounded-2xl hover:bg-amber-700 text-white font-bold">
+          Click Me!
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App
