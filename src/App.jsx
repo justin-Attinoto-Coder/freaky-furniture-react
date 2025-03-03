d2d5d10eafdb40c80e98cf97b5b25403a667388f
Melanie Farspour <melaniefars@gmail.com>
2025-03-03 13:38:10 -0800

    Lagt till productdetails page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import ProductDetails from './pages/productdetails';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/product-details" element={<ProductDetails />} />
       </Routes>
       <Footer />
     </Router>
