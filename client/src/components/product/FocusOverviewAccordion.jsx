36221c9db49526d0654b71fc020eb4607c454fbf
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-03-19 16:50:02 +0100

    Refactor OverviewAccordionItem component into FocusOverviewAccordion; remove old OverviewAccordionItem file

--- client/src/components/product/FocusOverviewAccordion.jsx
+++ client/src/components/product/FocusOverviewAccordion.jsx
@@ -1,5 +1,38 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import OverviewAccordionItem from './OverviewAccordionItem';
+
+const OverviewAccordionItem = ({ title, children, customClass }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`border-b ${customClass}`}>
+      <button
+        className={`w-full text-left p-4 focus:outline-none ${customClass}`}
+        onClick={toggleAccordion}
+      >
+        <div className="flex justify-between items-center">
+          <span>{title}</span>
+          <span>{isOpen ? '-' : '+'}</span>
+        </div>
+      </button>
+      {isOpen && <div className="p-4 bg-white">{children}</div>}
+    </div>
+  );
+};
+
+OverviewAccordionItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  customClass: PropTypes.string,
+};
+
+OverviewAccordionItem.defaultProps = {
+  customClass: '',
+};
 
 const FocusOverviewAccordion = ({ product, reviews = [] }) => {
   return (
