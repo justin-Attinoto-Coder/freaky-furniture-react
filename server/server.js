2f68546407846cf87c2abe03989fe592f236c957
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-04-20 20:23:38 +0200

    Fix static file serving path in server configuration

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client/public/')));
+app.use(express.static(path.join(__dirname, 'client/public/images')));
 app.use(bodyParser.json());
 
 // Use routes
