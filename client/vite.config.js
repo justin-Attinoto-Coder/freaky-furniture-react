70e82aa73f73df7c8150e9961cee012a5b9d7b93
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-22 13:55:55 +0200

    fix: update Tailwind CSS plugin import path in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from 'vite-plugin-tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
