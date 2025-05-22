33c2db7cc18f013330f55997d3b198cd67e9054b
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-22 08:57:36 +0200

    Fix import path casing for AddToCartButton in ProductInformation.jsx

--- client/src/components/product/ProductInformation.jsx
+++ client/src/components/product/ProductInformation.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import AddToCartButton from '../Cart/AddToCartButton';
+import AddToCartButton from '../cart/AddToCartButton';
 
 const ProductInformation = ({ title, brand, description, price, onAddToCart }) => {
   return (
