abceb1aa7cf445221677b1188fe74bb82fc23e3f
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-05-23 05:43:53 +0200

    Update Hero.jsx

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -3,7 +3,7 @@ import '../../../src/index.css'; // Import your CSS file
 const Hero = () => {
     return (
         <div className="hero-container">
-            <img src=".https://freaky-furniture-react-server.onrender.com/images/hero-one.jfif" alt="hero" />
+            <img src="/images/hero-one.jfif" alt="hero" />
             <div className="hero-content">
                 <h1>Hero Section</h1>
                 <p>Your one-stop shop for unique and stylish furniture</p>
